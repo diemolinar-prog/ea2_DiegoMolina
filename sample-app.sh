977465df05bdb4fe949d7bf0d7d31b492d8c5c9e
diemolinar-prog <changeme@example.com>
2026-05-05 22:22:45 +0000

    Se cambió el puerto de 8080 a 5050.

--- sample-app.sh
+++ sample-app.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+<<<<<<< HEAD
+=======
+rm -rf tempdir
+>>>>>>> Se cambió el puerto de 8080 a 5050.
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
@@ -9,6 +13,7 @@ cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
 echo "FROM python" >> tempdir/Dockerfile
+<<<<<<< HEAD
 echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
@@ -20,3 +25,18 @@ cd tempdir
 docker build -t sampleapp .
 docker run -t -d -p 8080:8080 --name samplerunning sampleapp
 docker ps -a 
+=======
+echo "RUN python3 -m pip install flask --no-cache-dir --progress-bar off" >> tempdir/Dockerfile
+echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "EXPOSE 5050" >> tempdir/Dockerfile
+echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
+
+cd tempdir
+
+docker rm -f samplerunning 2>/dev/null
+docker build -t sampleapp .
+docker run -t -d -p 5050:5050 --name samplerunning sampleapp
+docker ps -a
+>>>>>>> Se cambió el puerto de 8080 a 5050.
--- tempdir/sample_app.py
+++ tempdir/sample_app.py
@@ -10,8 +10,4 @@ def main():
     return render_template("index.html")
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    sample.run(host="0.0.0.0", port=8080)
-=======
     sample.run(host="0.0.0.0", port=5050, threaded=False)
->>>>>>> Se cambió el puerto de 8080 a 5050.
